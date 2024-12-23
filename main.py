521de41d201712a6d33b3dfe946f600515ba5057
sanvi-2014girl <goswamilipi82@gmail.com>
2024-12-23 11:48:04 +0530

    Parenthesis

---
+++ main.py
@@ -0,0 +1,15 @@
+def paren(s,l,r,p,n):
+    if(p==2*n):
+        for ss in s:
+            print(ss,end='')
+        print("\n")
+        return
+    if(l>r):
+        s[p]="}"
+        paren(s,l+1,r,p+1,n)
+    if(l<n):
+        paren(s,l+1,r,p+1,n)
+n = int(input("Enter number of parenthesis: "))
+s = [""]*2*n
+print("\n")
+paren(s,0,0,0,n)
